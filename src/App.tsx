c29d084019da965f1fef07b77efaa028a021e277
Kenny Nguyen <sokenns@gmail.com>
2025-02-10 19:08:45 -0800

    Move Canvas element into container element

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { Canvas } from "@react-three/fiber";
+// import Circle from "./components/Circle";
 import Sphere from "./components/Sphere";
 
 function App() {
+
+
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Sphere position={[-1.2, 0, 0]} />
-      <Sphere position={[1.2, 0, 0]} />
-    </Canvas>
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Sphere />
+        {/* <Circle /> */}
+      </Canvas>
+    </div>
   );
 }
 
