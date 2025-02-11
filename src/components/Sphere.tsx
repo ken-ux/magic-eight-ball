d66286c47061783ec6876c25b42d849d21586da1
Kenny Nguyen <sokenns@gmail.com>
2025-02-11 11:27:08 -0800

    Remove displacement map, add limited rotation to sphere

--- src/components/Sphere.tsx
+++ src/components/Sphere.tsx
@@ -1,8 +1,7 @@
 import { ThreeElements, useFrame, useLoader } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import * as THREE from "three";
-import color from "../assets/textures/color.jpg";
-import displacement from "../assets/textures/displacement.jpg";
+import color from "../assets/textures/ex.jpg";
 import metalness from "../assets/textures/metalness.jpg";
 import normal from "../assets/textures/normal.jpg";
 import roughness from "../assets/textures/roughness.jpg";
@@ -11,11 +10,17 @@ function Sphere(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-  useFrame((_state, delta) => (clicked ? ref.current.rotation.x += delta : ref.current.rotation.x -= delta));
+  useFrame((_state, delta) => {
+    if (clicked && ref.current.rotation.x <= THREE.MathUtils.degToRad(180)) {
+      ref.current.rotation.x += delta;
+    } else if (!clicked && ref.current.rotation.x >= 0) {
+      ref.current.rotation.x -= delta;
+    }
+  });
 
-  const [colorMap, displacementMap, metalnessMap, normalMap, roughnessMap] = useLoader(
+  const [colorMap, metalnessMap, normalMap, roughnessMap] = useLoader(
     THREE.TextureLoader,
-    [color, displacement, metalness, normal, roughness]
+    [color, metalness, normal, roughness]
   );
 
   return (
@@ -27,13 +32,12 @@ function Sphere(props: ThreeElements["mesh"]) {
       onPointerOver={() => hover(true)}
       onPointerOut={() => hover(false)}
     >
-      <sphereGeometry args={[1, 64, 32]} />
+      <sphereGeometry args={[2, 64, 32]} />
       <meshStandardMaterial
         color={hovered ? "hotpink" : "orange"}
         // wireframe={true}
-        displacementScale={0.2}
+        displacementScale={0}
         map={colorMap}
-        displacementMap={displacementMap}
         metalnessMap={metalnessMap}
         normalMap={normalMap}
         roughnessMap={roughnessMap}
