188488b6ebb8792d2447ff53b57a59a628b8ad90
Kenny Nguyen <sokenns@gmail.com>
2025-02-18 11:53:55 -0800

    Remove hover state, reduce vertex positions

--- src/components/Triangle.tsx
+++ src/components/Triangle.tsx
@@ -5,28 +5,26 @@ import * as THREE from "three";
 
 function Triangle(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
-  const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
   // useFrame((_state, delta) => (ref.current.rotation.x += delta));
 
   const vertices = [
-    new THREE.Vector2(0, 1), // Top vertex
-    new THREE.Vector2(-1, -0.5), // Left vertex
-    new THREE.Vector2(1, -0.5), // Right vertex
+    new THREE.Vector2(0, 0.75), // Top vertex
+    new THREE.Vector2(-0.5, 0), // Left vertex
+    new THREE.Vector2(0.5, 0), // Right vertex
   ];
 
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 2 : 1}
+      scale={1}
+      rotation={[0, 0, 0]}
       onClick={() => click(!clicked)}
-      onPointerOver={() => hover(true)}
-      onPointerOut={() => hover(false)}
     >
       {/* <bufferGeometry setFromPoints={vertices}/> */}
       <shapeGeometry args={[new THREE.Shape(vertices)]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      <meshStandardMaterial color={clicked ? "hotpink" : "orange"} />
     </mesh>
   );
 }
